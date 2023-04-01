6895c8e969c7815836dbc162d7f6b9d82b290963
saikalyanuppala <saikalyan.uppala@gmail.com>
2023-04-01 16:20:50 +0530

    initial commit

---
+++ src/test/java/com/kalyan/devops/DevopsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalyan.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
